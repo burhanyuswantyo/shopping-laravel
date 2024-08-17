2fff29278ea90055942d61b60302de93185bab52
Burhan Yuswantyo <55840266+burhanyuswantyo@users.noreply.github.com>
2024-08-17 15:40:25 +0700

    Routes

--- routes/api.php
+++ routes/api.php
@@ -1,5 +1,11 @@
 <?php
 
+use App\Http\Controllers\Api\AuthController;
+use App\Http\Controllers\Api\CartController;
+use App\Http\Controllers\Api\CategoryController;
+use App\Http\Controllers\Api\OrderController;
+use App\Http\Controllers\Api\PaymentController;
+use App\Http\Controllers\Api\ProductController;
 use Illuminate\Http\Request;
 use Illuminate\Support\Facades\Route;
 
@@ -14,6 +20,24 @@ use Illuminate\Support\Facades\Route;
 |
 */
 
-Route::middleware('auth:sanctum')->get('/user', function (Request $request) {
-    return $request->user();
+Route::middleware('auth:sanctum')
+    ->get('/user', function (Request $request) {
+        return $request->user();
+    });
+
+Route::get('products/get-best-seller', [ProductController::class, 'getBestSellerProducts']);
+Route::get('cart/get-count/{id}', [CartController::class, 'getCount']);
+
+Route::apiResource('products', ProductController::class);
+Route::apiResource('categories', CategoryController::class);
+Route::apiResource('cart', CartController::class)->except('show');
+Route::apiResource('orders', OrderController::class)->except('destroy');
+Route::apiResource('payments', PaymentController::class)->only('show', 'store', 'update');
+
+Route::post('login', [AuthController::class, 'login']);
+Route::post('register', [AuthController::class, 'register']);
+
+Route::middleware('auth:sanctum')->group(function () {
+    Route::get('get-user', [AuthController::class, 'getUser']);
+    Route::post('logout', [AuthController::class, 'logout']);
 });
